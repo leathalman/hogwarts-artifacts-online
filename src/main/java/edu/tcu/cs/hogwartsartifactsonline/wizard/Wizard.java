07ea9d86edf7ee9879095fe3585f372f5f2b1dab
Harrison Leath <hleath@me.com>
2024-03-08 23:53:14 -0500

    End of video 22

--- src/main/java/edu/tcu/cs/hogwartsartifactsonline/wizard/Wizard.java
+++ src/main/java/edu/tcu/cs/hogwartsartifactsonline/wizard/Wizard.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -17,7 +18,7 @@ public class Wizard implements Serializable {
     private String name;
 
     @OneToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE}, mappedBy = "owner")
-    private List<Artifact> artifacts;
+    private List<Artifact> artifacts = new ArrayList<>();
 
     public Wizard() {
 
@@ -45,5 +46,10 @@ public class Wizard implements Serializable {
 
     public void setArtifacts(List<Artifact> artifacts) {
         this.artifacts = artifacts;
+    }
+
+    public void addArtifact(Artifact artifact) {
+        artifact.setOwner(this);
+        this.artifacts.add(artifact);
     }
 }
---
+++ src/test/java/edu/tcu/cs/hogwartsartifactsonline/artifact/ArtifactServiceTest.java
@@ -0,0 +1,90 @@
+package edu.tcu.cs.hogwartsartifactsonline.artifact;
+
+import edu.tcu.cs.hogwartsartifactsonline.wizard.Wizard;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class ArtifactServiceTest {
+
+    @Mock
+    ArtifactRepository artifactRepository;
+    @InjectMocks
+    ArtifactService artifactService;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void testFindByIdSuccess() {
+        // Given -> Fake data. Arrange inputs and targets. Define the behavior of Mock object artifactRepository.
+        /*
+        "id": "1250808601744904192",
+        "name": "Invisibility Cloak",
+        "description": "An invisibility cloak is used to make the wearer invisible.",
+        "imageUrl": "ImageUrl",
+         */
+        Artifact a = new Artifact();
+        a.setId("1250808601744904192");
+        a.setName("Invisibility Cloak");
+        a.setDescription("An invisibility cloak is used to make the wearer invisible.");
+        a.setImageUrl("ImageUrl");
+
+        Wizard w = new Wizard();
+        w.setId(2);
+        w.setName("Harry Potter");
+
+        a.setOwner(w);
+
+        // Defines the behavior of mock object
+        given(artifactRepository.findById("1250808601744904192")).willReturn(Optional.of(a));
+
+        // When -> Call method to be tested. Act on the target behavior. When steps should cover the method to be tested.
+        Artifact returnedArtifact = this.artifactService.findById("1250808601744904192");
+
+        // Then -> Compare results. Assert expected outcomes.
+        assertThat(returnedArtifact.getId()).isEqualTo(a.getId());
+        assertThat(returnedArtifact.getName()).isEqualTo(a.getName());
+        assertThat(returnedArtifact.getDescription()).isEqualTo(a.getDescription());
+        assertThat(returnedArtifact.getImageUrl()).isEqualTo(a.getImageUrl());
+        assertThat(returnedArtifact.getOwner()).isNotNull();
+        // verify that it is called exactly once
+        verify(this.artifactRepository, times(1)).findById("1250808601744904192");
+    }
+
+    @Test
+    void testFindByIdNotFound() {
+        // Given
+        given(artifactRepository.findById(Mockito.any(String.class))).willReturn(Optional.empty());
+
+        // When
+        Throwable thrown = catchThrowable(() -> {
+            Artifact returnedArtifact = this.artifactService.findById("1250808601744904192");
+        });
+
+        // Then
+        assertThat(thrown).
+                isInstanceOf(ArtifactNotFoundException.class)
+                .hasMessage("Could not find artifact with Id 1250808601744904192 :((");
+        verify(this.artifactRepository, times(1)).findById("1250808601744904192");
+    }
+}
