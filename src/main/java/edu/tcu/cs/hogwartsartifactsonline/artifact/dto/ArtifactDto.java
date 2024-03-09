b0405409d55723d804e2f5d33b33561f03bf64df
Harrison Leath <hleath@me.com>
2024-03-09 13:17:04 -0500

    Artifact CRUD done :) Close #1

--- src/main/java/edu/tcu/cs/hogwartsartifactsonline/artifact/dto/ArtifactDto.java
+++ src/main/java/edu/tcu/cs/hogwartsartifactsonline/artifact/dto/ArtifactDto.java
@@ -1,10 +1,14 @@
 package edu.tcu.cs.hogwartsartifactsonline.artifact.dto;
 
 import edu.tcu.cs.hogwartsartifactsonline.wizard.dto.WizardDto;
+import jakarta.validation.constraints.NotEmpty;
 
 public record ArtifactDto(String id,
-                         String name,
-                         String description,
-                         String imageUrl,
-                         WizardDto owner) {
+                          @NotEmpty(message = "name is required")
+                          String name,
+                          @NotEmpty(message = "description is required")
+                          String description,
+                          @NotEmpty(message = "imageurl is required")
+                          String imageUrl,
+                          WizardDto owner) {
 }
